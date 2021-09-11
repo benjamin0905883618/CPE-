5d69c92efce23abdb1a2bb3d58da4b8d2ca396e9
benjamin0905883618 <66818489+benjamin0905883618@users.noreply.github.com>
2021-09-11 14:31:11 +0800

    Add files via upload

--- uva10041.cpp
+++ uva10041.cpp
@@ -2,44 +2,26 @@
 #include<algorithm>
 using namespace std;
 
-int T;
-int t;
-int num;
-int *data;
-
-void init(){
-    cin >> T;
-    t = 0;
-}
-
-bool input(){
-    if(t++ == T)
-        return false;
-    cin >> num;
-    data = new int [num];
-    for(int i = 0;i < num;i++)
-        cin >> data[i];
-    sort(data,data + num);
-}
-
-int run(){
-    int median;
-    int sum = 0;
-    if(num % 2 == 0)
-        median = (data[num/2 - 1] + data[num/2]) / 2;
-    else
-        median = data[num/2];
-    for(int i = 0;i < num;i++){
-        if(data[i] < median)
-            sum += median - data[i];
-        else
-            sum += data[i] - median;
-    }
-    return sum;
-}
-
 int main(){
-    init();
-    while(input())
-        cout << run() << endl;
+    int cases;
+    cin >> cases;
+    for(int i = 0;i < cases; i++){
+        int relatives;
+        cin >> relatives;
+        int *street_num = new int [relatives];
+        for(int j = 0; j < relatives; j++){
+            cin >> street_num[j];
+        }
+        sort(street_num, street_num + relatives);
+        int middle = street_num[int(relatives / 2)];
+        int sum = 0;
+        for(int k = 0; k < relatives; k++){
+            if(middle > street_num[k])
+                sum += middle - street_num[k];
+            else{
+                sum += street_num[k] - middle;
+            }
+        }
+        cout << sum << endl;
+    }
 }
